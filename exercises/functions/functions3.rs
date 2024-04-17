a52bda64f74989ebb5a647dce094a74e740d6e03
Squanch <a246004219@gmail.com>
2024-04-17 21:49:54 +0800

    完成到String3，准备开始写字符串替换

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    let x = 3u32;
+    call_me(x);
 }
 
 fn call_me(num: u32) {
